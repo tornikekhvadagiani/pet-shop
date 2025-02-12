e00233dbcff65d1b3f99e238e5bca3b03350c632
tornikekhvadagiani <tornikexvadagiani1@gmail.com>
2025-02-13 00:38:11 +0400

    fixed routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,25 +1,19 @@
-import { Route } from "react-router-dom"
-import AnimalsPage from "./pages/AnimalsPage"
-import CategoryPage from "./pages/CategoryPage"
-import HomePage from "./pages/HomePage"
-import AppLayout from "./layout/AppLayout"
-
-
-
-
-
+import { Route, Routes } from "react-router-dom";
+import AnimalsPage from "./pages/AnimalsPage";
+import CategoryPage from "./pages/CategoryPage";
+import HomePage from "./pages/HomePage";
+import AppLayout from "./layout/AppLayout";
 
 const Router = () => {
   return (
-    <Route element={<AppLayout/>}>  
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="Animals" element={<AnimalsPage/>}/>
-          <Route path="category" element={<CategoryPage/>}/>
-       </Route> 
-   
-  )
-}
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="animals" element={<AnimalsPage />} />
+        <Route path="category" element={<CategoryPage />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default Router
-
-
+export default Router;
